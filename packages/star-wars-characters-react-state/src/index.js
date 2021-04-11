fe1679e90219f2368dd650c32e4e44b4e16064f0
Isaac Ramirez <iramirezc@live.com.mx>
2021-04-11 12:04:00 -0500

    Data Fetching: - implement useEffect to fetch data on first run

--- packages/star-wars-characters-react-state/src/index.js
+++ packages/star-wars-characters-react-state/src/index.js
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import CharacterList from './CharacterList';
-
 import dummyData from './dummy-data';
-
+import endpoint from './endpoint';
 import './styles.scss';
 
 const Application = () => {
   const [characters, setCharacters] = useState(dummyData);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setCharacters([]);
+    setIsLoading(true);
+    setError(null);
+
+    fetch(endpoint + '/characters')
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        setIsLoading(false);
+        setCharacters(response.characters || []);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setError(error);
+      });
+  }, []);
 
   return (
     <div className="Application">
@@ -18,9 +36,17 @@ const Application = () => {
         <h1>Star Wars Characters</h1>
       </header>
       <main>
-        <section className="sidebar">
-          <CharacterList characters={characters} />
-        </section>
+        {isLoading ? (
+          <h1>Loading...</h1>
+        ) : error ? (
+          <pre>
+            <code>Error: {error.message}</code>
+          </pre>
+        ) : (
+          <section className="sidebar">
+            <CharacterList characters={characters} />
+          </section>
+        )}
       </main>
     </div>
   );
